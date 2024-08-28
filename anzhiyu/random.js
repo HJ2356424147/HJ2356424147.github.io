2bc1945e61c396b1342bf31ea1ce16c1f4062def
HJ2356424147 <2356424147@qq.com>
2024-08-28 19:44:54 +0800

    Site updated: 2024-08-28 19:44:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/27/hello-world/"];function toRandomPost(){
+var posts=["2024/08/27/hello-world/","2024/08/28/改变如何开始/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
