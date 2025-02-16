a19adb25009595704087e76f07bccb8d2ec42b2e
HJ2356424147 <2356424147@qq.com>
2025-02-16 16:58:12 +0800

    Site updated: 2025-02-16 16:58:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/27/hello-world/","2024/08/28/改变如何开始/"];function toRandomPost(){
+var posts=["2024/08/27/hello-world/","2024/08/28/如何利用hexo搭建博客/","2025/02/16/学习java后端需要掌握的知识/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
